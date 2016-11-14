f7fddd6e38f36a697613aee807caa16e27f6570e
Christian Sciberras <christian@sciberras.me>
2016-11-14 12:46:51 +0100

    Created PhpCsFixer action

---
+++ src/ElderBrother/Action/PhpCsFixer.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace uuf6429\ElderBrother\Action;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use uuf6429\ElderBrother\Change\FileList;
+
+class PhpCsFixer extends ActionAbstract
+{
+    /**
+     * @var FileList
+     */
+    protected $files;
+
+    /**
+     * @var string|null
+     */
+    protected $configFile;
+
+    /**
+     * @var boolean
+     */
+    protected $addAutomatically;
+
+    /**
+     * Runs all the provided files through PHP-CS-Fixer, fixing any code style issues.
+     *
+     * @param FileList $files The files to check
+     * @param string|null $configFile (Optional, default is project root) File path to PHP-CS-Fixer config
+     * @param boolean $addAutomatically (Optional, default is true) Whether to add modified files to commit or not.
+     */
+    public function __construct(FileList $files, $configFile = null, $addAutomatically = true)
+    {
+        $this->files = $files;
+        $this->configFile = $configFile;
+        $this->addAutomatically = $addAutomatically;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'PHP Code Style Fixer (PhpCsFixer)';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function checkSupport()
+    {
+        return file_exists($this->getBinFile());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $files = $this->files->toArray();
+
+        $progress = $this->createProgressBar($input, $output, count($files));
+        $progress->start();
+
+        $failed = [];
+        $fixer = $this->getBinFile();
+
+        foreach ($files as $file) {
+            $progress->setMessage('Processing ' . $file . '...');
+
+            $outp = null;
+            $exit = null;
+            exec(
+                sprintf(
+                    'php -f %s fix %s %s',
+                    escapeshellarg($fixer),
+                    escapeshellarg($file),
+                    $this->configFile ? ('--config-file=' . escapeshellarg($this->configFile)) : ''
+                ),
+                $outp,
+                $exit
+            );
+
+            switch ((int) $exit) {
+                case 0:
+                    // file has been changed
+                    if ($this->addAutomatically) {
+                        exec('git add ' . escapeshellarg($file));
+                    }
+                    break;
+                case 1:
+                    // file not changed
+                    break;
+                default:
+                    // some sort of error
+                    $failed[$file] = $outp;
+                    break;
+            }
+
+            $progress->advance();
+        }
+
+        if (count($failed)) {
+            $message = 'PhpCsFixer failed for the following file(s):';
+            foreach ($failed as $file => $result) {
+                $message .= PHP_EOL . '- ' . $file . ':';
+                $message .= PHP_EOL . ' - ' . implode(PHP_EOL . ' - ', $result);
+            }
+            throw new \RuntimeException($message);
+        }
+
+        $progress->finish();
+    }
+
+    /**
+     * @return string
+     */
+    protected function getBinFile()
+    {
+        return PROJECT_ROOT . 'vendor' . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'php-cs-fixer';
+    }
+}
